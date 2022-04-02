d95cef5b03ec4572509c1f506e671edf271501ee
Sharath <sharathnair9999@gmail.com>
2022-04-02 19:31:30 +0530

    Added Empty Data illustration for no notes case

--- src/app-utils/constants.js
+++ src/app-utils/constants.js
@@ -10,5 +10,7 @@ export const constants = {
       "https://res.cloudinary.com/sharath-media-library/image/upload/v1648634741/note-app/new-user_qzszc7.svg",
     notes_landing:
       "https://res.cloudinary.com/sharath-media-library/image/upload/v1648879304/note-app/choose_files_iurga7.jpg",
+    empty_data_svg:
+      "https://res.cloudinary.com/sharath-media-library/image/upload/v1647631255/nothing-store-project/empty_data_r69ppl.svg",
   },
 };

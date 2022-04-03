2c59e31eeeab66db203d92eec032e846e5a35dcb
Sharath <sharathnair9999@gmail.com>
2022-04-04 00:34:11 +0530

    Fix: CSS

--- src/pages/NotesPage/NotesPage.js
+++ src/pages/NotesPage/NotesPage.js
@@ -5,46 +5,58 @@ import "./NotesPage.css";
 const NotesPage = () => {
   return (
     <div className="notes-page-section gap-2">
-      <nav className="notes-nav-section">
+      <nav className="notes-nav-section show">
         <ul className="notes-ul flex justify-fs items-fs flex-col w-100">
           <li>
             <NavLink
               className={({ isActive }) =>
-                `${isActive ? "active-link":"inactive-link"} w-100 flex justify-fs items-center gap-sm`
+                `${
+                  isActive ? "active-link" : "inactive-link"
+                } w-100 flex justify-fs items-center gap-sm`
               }
               to={"all-notes"}
             >
-             <i className="fa-solid fa-note-sticky"></i><span>Notes</span>
+              <i className="fa-solid fa-note-sticky"></i>
+              <span>Notes</span>
             </NavLink>
           </li>
           <li>
             <NavLink
               className={({ isActive }) =>
-              `${isActive ? "active-link":"inactive-link"} w-100 flex justify-fs items-center gap-sm `
+                `${
+                  isActive ? "active-link" : "inactive-link"
+                } w-100 flex justify-fs items-center gap-sm `
               }
               to={"label"}
             >
-              <i className="fa-solid fa-box-archive"></i><span>Label</span>
+              <i className="fa-solid fa-box-archive"></i>
+              <span>Label</span>
             </NavLink>
           </li>
           <li>
             <NavLink
               className={({ isActive }) =>
-                `${isActive ? "active-link":"inactive-link"} w-100 flex justify-fs items-center gap-sm `
+                `${
+                  isActive ? "active-link" : "inactive-link"
+                } w-100 flex justify-fs items-center gap-sm `
               }
               to={"archive"}
             >
-              <i className="fa-solid fa-tag"></i><span>Archive</span>
+              <i className="fa-solid fa-tag"></i>
+              <span>Archive</span>
             </NavLink>
           </li>
           <li>
             <NavLink
               className={({ isActive }) =>
-                `${isActive ? "active-link":"inactive-link"} w-100 flex justify-fs items-center gap-sm `
+                `${
+                  isActive ? "active-link" : "inactive-link"
+                } w-100 flex justify-fs items-center gap-sm `
               }
               to={"trash"}
             >
-              <i className="fa-solid fa-trash-can"></i><span>Trash</span>
+              <i className="fa-solid fa-trash-can"></i>
+              <span>Trash</span>
             </NavLink>
           </li>
         </ul>

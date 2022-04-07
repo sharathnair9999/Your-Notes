1d0dd090c998e0b14e4af67680733c194a14d935
Sharath <sharathnair9999@gmail.com>
2022-04-07 10:51:34 +0530

    Feat : Error Page

--- src/pages/ErrorPage/ErrorPage.js
+++ src/pages/ErrorPage/ErrorPage.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { constants } from "../../app-utils/constants";
+import "./ErrorPage.css";
 
 const ErrorPage = () => {
   return (
-    <div>ErrorPage</div>
-  )
-}
+    <div className="error-page-section">
+      <h1>Page Not Found</h1>
+      <div className="error-img-container">
+        <img
+          src={constants.imgUrls.error_404}
+          alt="error  4040 page"
+          className="responsive-img"
+        />
+      </div>
+    </div>
+  );
+};
 
-export default ErrorPage
+export default ErrorPage;

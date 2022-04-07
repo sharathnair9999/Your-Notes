5773304e5ebaf7103b1982cc9cccfa04d41ff604
Sharath <sharathnair9999@gmail.com>
2022-04-07 11:02:14 +0530

    Home Button

--- src/pages/ErrorPage/ErrorPage.js
+++ src/pages/ErrorPage/ErrorPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { constants } from "../../app-utils/constants";
 import "./ErrorPage.css";
 
@@ -6,6 +7,7 @@ const ErrorPage = () => {
   return (
     <div className="error-page-section">
       <h1>Page Not Found</h1>
+      <Link className="btn btn-primary" to={"/notes"}>Go To Home</Link>
       <div className="error-img-container">
         <img
           src={constants.imgUrls.error_404}
